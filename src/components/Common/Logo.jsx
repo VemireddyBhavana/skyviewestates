57c5deaa3f3ff6e45b0e9e63eaff0f18e1f682f3
Vemireddy Bhavana <vemireddybhavana@example.com>
2026-06-22 14:02:09 +0530

    Update logo to use Cloudinary image URL

--- src/components/Common/Logo.jsx
+++ src/components/Common/Logo.jsx
@@ -1,10 +1,9 @@
 import { Link } from 'react-router-dom';
-import logoImg from '../../assets/logo.png';
 
 const Logo = ({ className }) => (
   <Link to="/" className={`nav-logo ${className}`} style={{ textDecoration: 'none', color: 'inherit' }}>
     <div className="logo-container">
-      <img src={logoImg} alt="Sun Bright Properties Icon" className="logo-icon-img" style={{ height: '45px', width: 'auto', objectFit: 'contain' }} />
+      <img src="https://res.cloudinary.com/djzgjy947/image/upload/v1782116843/Screenshot_2026-06-01_090635_wodt1g.png" alt="Sun Bright Properties Icon" className="logo-icon-img" style={{ height: '45px', width: 'auto', objectFit: 'contain' }} />
       <div className="logo-text">
         <span className="brand-skyview" style={{ color: 'currentColor' }}>SUN BRIGHT PROPERTIES</span>
         <span className="brand-estates" style={{ color: '#D4AF37' }}>THE CHAMPIONS CHOICE</span>
